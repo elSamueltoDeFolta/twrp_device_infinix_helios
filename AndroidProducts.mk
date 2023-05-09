a839f93cac7098a17ad523fb9feb9002fc69934a
ZunayedDihan <zunayeddihan@gmail.com>
2023-05-09 23:11:38 +0600

    x6827: twrp>> omni

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X6827.mk
+    $(LOCAL_DIR)/twrp_Infinix-X6827.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X6827-user \
-    omni_Infinix-X6827-userdebug \
-    omni_Infinix-X6827-eng
+    twrp_Infinix-X6827-user \
+    twrp_Infinix-X6827-userdebug \
+    twrp_Infinix-X6827-eng
--- twrp_Infinix-X6827.mk
+++ twrp_Infinix-X6827.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/infinix/Infinix-X6827/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6827
-PRODUCT_NAME := omni_Infinix-X6827
+PRODUCT_NAME := twrp_Infinix-X6827
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6827
 PRODUCT_MANUFACTURER := infinix
