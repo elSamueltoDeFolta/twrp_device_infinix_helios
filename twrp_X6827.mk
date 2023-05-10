994d78215074dc7d2e6da4191214d2766285da67
lopestorm <guillopes2002@gmail.com>
2023-05-10 19:29:26 -0300

    Update twrp_X6827.mk
    - Without reference from G loke aosp_base.mk or base.mk the img can't compile.
    
    - Adc for more GSI compatible
    Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.

--- twrp_X6827.mk
+++ twrp_X6827.mk
@@ -8,6 +8,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -19,7 +23,7 @@ PRODUCT_DEVICE := X6827
 PRODUCT_NAME := twrp_X6827
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6827
-PRODUCT_MANUFACTURER := infinix
+PRODUCT_MANUFACTURER := Infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
 
