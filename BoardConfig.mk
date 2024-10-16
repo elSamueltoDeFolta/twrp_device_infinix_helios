3e12d7678537287f24e640b8a17905e08a0d3645
╚»«╝ El'XiaoYinDeHioa ╚»«╝ <budierik457@gmail.com>
2024-10-16 19:38:05 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,7 @@ BOARD_USES_MTK_HARDWARE := true
 
 ## TWRP-Specific configuration
 TW_THEME := portrait_hdpi
-TW_DEVICE_VERSION := IMY
+TW_DEVICE_VERSION := ElHiaoYinDeXioa V1
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_USES_MKE2FS := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
